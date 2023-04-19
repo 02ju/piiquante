9bde4a84ae62aa494a65487d0c1acc192c8be709
Emine Erkinbekov <eminechem@gmail.com>
2023-04-19 10:52:04 +0200

    first commit

---
+++ backend/routes/sauces.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const saucesCtrl = require('../controllers/sauces');
+
+router.get('/', auth, saucesCtrl.getAllSauces);
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce); 
+
+module.exports = router;
+
+/* avant auth
+router.get('/', stuffCtrl.getAllStuff);
+router.post('/', stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing); 
+*/
